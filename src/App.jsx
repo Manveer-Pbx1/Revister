c9240d2f7208dcaa38b049effc5cdaf7c9d2682a
Manveer <manveer7saggu@gmail.com>
2024-07-02 20:41:52 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Components/Header'
+import List from './Components/List'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <List/>
+    </div>
+  )
+}
