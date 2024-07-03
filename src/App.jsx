462b0449ef73cd4efae502744bb37c607b518abc
Manveer <manveer7saggu@gmail.com>
2024-07-04 01:18:34 +0530

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import Header from './Components/Header'
 import List from './Components/List'
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Notifications from './Components/Notifications';
 export default function App() {
   return (
+    <Router>
     <div>
-      <Header/>
-      <List/>
+      <Routes>
+      <Route path='/' element= {<List/> }/>
+      <Route path='/notifications' element= {<Notifications/>}/>
+      </Routes>
     </div>
+    </Router>
   )
 }
