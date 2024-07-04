4efe880c37e137c029f4b96f9489b2afa5ae61ac
Manveer <manveer7saggu@gmail.com>
2024-07-04 20:27:50 +0530

    Fixed email error

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const schedule = require("node-schedule");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,35 +10,41 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/send-notification', async (req, res) => {
+app.post("/send-notification", (req, res) => {
   const { email, time } = req.body;
 
   // Configure the transport options
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
-      user: 'manveer7saggu@gmail.com',
-      pass: 'enia dsxp xsvb exkv',
+      user: "revistergetconsistent@gmail.com",
+      pass: "aspy lwxf xsbd wcty",
     },
   });
 
   // Define email options
   let mailOptions = {
-    from: 'manveer7saggu@gmail.com',
+    from: "revistergetconsistent@gmail.com",
     to: email,
-    subject: 'Notification Setup',
-    text: `You are required to revisit your problems! ${time}.`,
+    subject: "Notification Setup",
+    html: `<div style="padding: 24px">
+    <h1> Greetings from <strong><i>REVISTER</i></strong></h1>
+    <p>Time to revisit those problems!</p>
+            </div>`,
   };
 
-  try {
-    // Send the email
-    await transporter.sendMail(mailOptions);
-    res.status(200).send('Notification email sent');
-  } catch (error) {
-    res.status(500).send('Error sending email: ' + error);
-  }
+  // Schedule the email
+  const date = new Date(time);
+  schedule.scheduleJob(date, async () => {
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("Notification email sent");
+    } catch (error) {
+      console.error("Error sending email: ", error);
+    }
+  });
+
+  res.status(200).send("Notification email scheduled");
 });
 
 app.listen(PORT, () => {
