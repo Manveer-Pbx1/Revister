2949e4f88e80e1e4cf16169896c9708b8158c3f1
Manveer-Pbx1 <manveer7saggu@gmail.com>
2025-07-23 16:23:34 +0530

    changed api url

--- frontend/src/Notifications.jsx
+++ frontend/src/Notifications.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const API_URL = "http://localhost:5000";
+const API_URL = "https://revister-1.onrender.com";
 
 
 export default function Notifications() {
